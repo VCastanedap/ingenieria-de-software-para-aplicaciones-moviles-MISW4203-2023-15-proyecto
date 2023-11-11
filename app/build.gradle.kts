9042f39f50ab228db0ae14a34fd8acb77d852c9c
Edwin Tijaro <e.tijaro@uniandes.edu.co>
2023-11-11 03:10:09 -0500

    Se crean pruebas de lista de albums

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,12 +79,13 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test:core:1.4.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
 }
---
+++ app/src/androidTest/java/com/appbajopruebas/vinilos/ui/activities/testAlbum/Test_List_Album.java
@@ -0,0 +1,66 @@
+package com.appbajopruebas.vinilos.ui.activities.testAlbum;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.appbajopruebas.vinilos.MainActivity;
+import com.appbajopruebas.vinilos.R;
+import com.appbajopruebas.vinilos.VinilosActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class Test_List_Album {
+
+    @Before
+    public void launchActivity() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), VinilosActivity.class);
+        ActivityScenario.launch(intent);
+    }
+
+
+
+ @Test
+     public void testFragmento() {
+
+     try {
+         Thread.sleep(2000);
+     } catch (InterruptedException e) {
+         e.printStackTrace();
+     }
+     // Verifica que el item está presente y visible
+     onView(withId(R.id.albumsRv))
+             .check(matches(isDisplayed()));
+
+     // Hace scroll
+     onView(withId(R.id.albumsRv))
+             .perform(swipeUp());
+
+    }
+
+    @Test
+    public void testBackButton() {
+        // Presiona el botón de retroceso
+        onView(withContentDescription("Navigate up")).perform(click());
+
+        // Verifica que VinilosActivity está cerrada (no visible)
+        Espresso.onView(ViewMatchers.withId(R.id.minicard1))
+                .check(matches(isDisplayed()));
+    }
+ }
