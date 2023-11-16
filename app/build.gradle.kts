53e95eb5d0413ba47e4ff69b25e9637a77f7c645
Edwin Tijaro <e.tijaro@uniandes.edu.co>
2023-11-15 22:41:12 -0500

    Se agrega prueba de lista_Collector, Se prueba y ajustan Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,6 @@ android {
     namespace = "com.appbajopruebas.vinilos"
     compileSdk = 34
 
-
-
-
     defaultConfig {
         applicationId = "com.appbajopruebas.vinilos"
         minSdk = 21
@@ -49,12 +46,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
-
-
-
 }
-
-
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
@@ -79,7 +71,6 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
