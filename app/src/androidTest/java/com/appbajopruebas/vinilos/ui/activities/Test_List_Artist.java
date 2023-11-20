5046bc0235d16f6716ead2745b6c955995eb416d
Juan Manuel González Garzón <jm.gonzalez1844@uniandes.edu.co>
2023-11-19 23:43:15 -0500

    Pruebas para la lista de artistas

---
+++ app/src/androidTest/java/com/appbajopruebas/vinilos/ui/activities/Test_List_Artist.java
@@ -0,0 +1,51 @@
+package com.appbajopruebas.vinilos.ui.activities;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.matcher.ViewMatchers;
+
+import com.appbajopruebas.vinilos.ListArtistasActivity;
+import com.appbajopruebas.vinilos.R;
+import com.appbajopruebas.vinilos.VinilosActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test_List_Artist {
+
+    @Before
+    public void launchActivity() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), ListArtistasActivity.class);
+        ActivityScenario.launch(intent);
+    }
+
+ @Test
+     public void testFragmento() {
+
+     try {
+         Thread.sleep(2000);
+     } catch (InterruptedException e) {
+         e.printStackTrace();
+     }
+     // Verifica que el item está presente y visible
+     onView(withId(R.id.artistsRv))
+             .check(matches(isDisplayed()));
+
+     // Hace scroll
+     onView(withId(R.id.artistsRv))
+             .perform(swipeUp());
+
+    }
+
+ }
