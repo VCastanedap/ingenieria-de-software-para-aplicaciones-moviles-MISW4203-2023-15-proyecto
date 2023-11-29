742fab9bec948d11c475c2b7dd29f8c3c402251b
Edwin Tijaro <e.tijaro@uniandes.edu.co>
2023-11-29 01:29:42 -0500

    Desarrollo detalle Album

--- app/src/main/java/com/appbajopruebas/vinilos/fragment/AlbumFragment.kt
+++ app/src/main/java/com/appbajopruebas/vinilos/fragment/AlbumFragment.kt
@@ -1,17 +1,22 @@
 package com.appbajopruebas.vinilos.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.appbajopruebas.vinilos.MainActivity
 import com.appbajopruebas.vinilos.R
+import com.appbajopruebas.vinilos.VinilosActivity
 import com.appbajopruebas.vinilos.database.VinylRoomDatabase
 import com.appbajopruebas.vinilos.databinding.FragmentAlbumBinding
 import com.appbajopruebas.vinilos.models.Album
@@ -43,10 +48,26 @@ class AlbumFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         recyclerView = binding.albumsRv
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = viewModelAdapter
         Log.d("*** fragment","recyclerView " )
+
+// Configurar el listener para el botón de retroceso
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Acciones a realizar al presionar el botón de retroceso
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
+        // Habilitar el botón de retroceso en la barra de acciones
+        val mainActivity = requireActivity() as VinilosActivity
+        mainActivity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mainActivity.supportActionBar?.setDisplayShowHomeEnabled(true)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -79,4 +100,6 @@ class AlbumFragment : Fragment() {
             viewModel.onNetworkErrorShown()
         }
     }
+
+
 }
